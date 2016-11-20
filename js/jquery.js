755a8f2bdf87454b3180096879f1af8b1d284d1e
Safarali <mr.safarali@gmail.com>
2016-11-19 17:58:33 -0800

    Top-nav and header done,TypeJS added

---
+++ js/jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(function(){
+        $("#typing_text").typed({
+            strings: ["Think We Learn...", "Think We Build...", " Think We Share...", "Think WeCode!"],
+            typeSpeed: 25,
+            backSpeed: 0,
+            backDelay: 1000,
+            startDelay: 50,
+            loop: true,
+            loopCount: 3
+        });
+    });
+});
